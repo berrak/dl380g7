80fad85c12ed95e184ea43322c46578f88468271
berrak <bkronmailbox-git@yahoo.se>
2014-08-17 15:27:20 +0200

    Add install debs module

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,32 +6,25 @@ $extlookup_datadir = "/etc/puppet/files"
 
 node 'node-hphome.home.tld' {
 
+	## BASIC
+	
 	# Puppet helper routines
     include puppet_utils
-	# manage puppet itself
+	# Manage puppet itself
     include hp_puppetize
-	
-	include hp_screen
 	
 	# Configure APT
     include hp_aptconf
-	
-	# SSH server
-	include hp_ssh_server
-	
-	# Disable ipv6 in kernel/grub and use the more text lines in console mode	
-    class { hp_grub::install : defaultline => 'vga=791', appendline => 'true', ipv6 => 'false' }
-	
+		
 	# Lan ntp server provids time services to all lan clients
     class { 'hp_ntp' : role => 'lanserver', peerntpip => '192.168.0.12' }
 
-    # Set up root's home directories and bash customization
-    include hp_root_home
-    include hp_root_bashrc
-	
 	# hosts and fstab files
 	class { hp_hosts::config : puppetserver_hostname => 'hphome' }
 	class { hp_fstab::config : fstabhost => 'hphome' }
+	
+	
+	## SECURITY
 	
 	# Security (iptables + fail2ban)
 	# fail2ban ssh is enabled. disabled apache, modsec, postfix actions
@@ -43,10 +36,31 @@ node 'node-hphome.home.tld' {
 		       fail2ban_modsec => 'false',
 			  fail2ban_postfix => 'false',
 	}
-    # ASutomatic security upgrades with cron script
+    # Automatic security upgrades with cron script
 	include hp_auto_upgrade
 	
-	# Mail server, relay external mails via google
+	# Disable ipv6 in kernel/grub and use the more text lines in console mode	
+    class { hp_grub::install : defaultline => 'vga=791', appendline => 'true', ipv6 => 'false' }
+	
+	
+	## USER PROFILES ##
+	
+	# Set up root's home directories and bash customization
+    include hp_root_home
+    include hp_root_bashrc
+	
+	# Set up user's home directories and bash customization
+    hp_user_bashrc::config { 'bekr' : }
+	hp_sudo::config { 'bekr': }
+	hp_mutt::install { 'bekr': mailserver_hostname => 'hphome' }
+	
+	
+	## APPLICATIONS ##
+	
+	# Packages without any special configurations
+    class { hp_install_debs : debs => [ "tree", "sipcalc" ] }
+	
+	# MAIL server, relay external mails via google
 		hp_postfix::install { 'mta' :
 			            ensure => 'installed',
 			install_cyrus_sasl => 'true',
@@ -57,10 +71,14 @@ node 'node-hphome.home.tld' {
 		  no_lan_outbound_mail => 'false',
 	}
 	
-	# User profiles
-    hp_user_bashrc::config { 'bekr' : }
-	hp_sudo::config { 'bekr': }
-	hp_mutt::install { 'bekr': mailserver_hostname => 'hphome' }
+	
+	## MAINTENANCE
+	
+	# SSH server - Todo: change conf from pwd to RSA only
+	include hp_ssh_server
+	
+	include hp_screen
+	
 
 }
 #
---
+++ modules/hp_install_debs/manifests/init.pp
@@ -0,0 +1,15 @@
+#
+# Simple module to install Debian packages which
+# does not require any complicated setups
+#
+class hp_install_debs ( $debs ='' ) {
+
+    if $debs == '' {
+        fail("FAIL: Missing list of Debian package names ($debs) to install!")
+    }
+
+    # debs is an array of deb package names, defined in site.pp
+    
+    package { $debs : ensure => installed }
+
+}
