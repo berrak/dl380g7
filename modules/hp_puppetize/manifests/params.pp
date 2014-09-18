f2104174bf609849dce73c56d5c0ede997a3dfbb
berrak <bkronmailbox-git@yahoo.se>
2014-09-18 20:32:32 +0200

    Disable puppet agent daemon mode

--- modules/hp_puppetize/manifests/params.pp
+++ modules/hp_puppetize/manifests/params.pp
@@ -4,7 +4,8 @@
 class hp_puppetize::params {
     
     # List all hosts which also acts as Puppet server on same host as client
+    # Note: only used if puppet agent is running (in daemon mode)
 
-    $list_puppetservers_fqdn = [ 'hphome.home.tld', 'hpone.debinix.org' ]
+    $list_puppetservers_fqdn = [ 'hphome.home.tld', 'dl380g7.debinix.org' ]
 
 }
