0375aa93edbbd1e3c36bffb3212ab2c362fead0d
berrak <bkronmailbox-git@yahoo.se>
2014-10-04 14:15:44 +0200

    No automatic activation of alternative iptable

--- modules/hp_iptables_rpm/manifests/config.pp
+++ modules/hp_iptables_rpm/manifests/config.pp
@@ -32,20 +32,15 @@ class hp_iptables_rpm::config  {
 			require => File["/root/bin/fw.${myhostname}"],
 	}
 	
-	# iptables file for initial setup
+	# iptables file for initial setup (manual activation)
 	file { '/root/bin/fw.init' :
 		 source => "puppet:///modules/hp_iptables_rpm/fw.init",
 		  owner => 'root',
 		  group => 'root',
 		   mode => '0600',
-		 notify => Exec['activate_initial_iptable'],
 	}
 	
-	exec { 'activate_initial_iptable':
-			command => '/bin/bash /root/bin/fw.init',
-		refreshonly => true,
-			require => File['/root/bin/fw.init'],
-	}
+
         
 
 
