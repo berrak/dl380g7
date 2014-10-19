360f9bdc4e0df1d67ea871c72e61afe05683ef3b
berrak <bkronmailbox-git@yahoo.se>
2014-10-19 12:21:36 +0200

    Fix syntax

--- modules/hp_network_rpm/manifests/config.pp
+++ modules/hp_network_rpm/manifests/config.pp
@@ -2,7 +2,7 @@
 # Manage network
 #
 class hp_network_rpm::config ( $interface='', $ip='', $prefix='', 
-                $gateway='', $broadcast='', $ispdns1='', $ispdns2='', ) {
+                $gateway='', $broadcast='', $ispdns1='', $ispdns2='' ) {
 
 	include hp_network_rpm
 	
@@ -12,18 +12,18 @@ class hp_network_rpm::config ( $interface='', $ip='', $prefix='',
 	
 	$host_interface = $interface
 
-	$host-ip = $ip
-    $host-prefix = $prefix
-	$host-gateway = $gateway
-	$host-broadcast = $broadcast,
-	$host-dns1 = $ispdns1
-	$host-dns2 = $ispdns2	
+	$host_ip = $ip
+    $host_prefix = $prefix
+	$host_gateway = $gateway
+	$host_broadcast = $broadcast
+	$host_dns1 = $ispdns1
+	$host_dns2 = $ispdns2	
 
-	$host-mac = $::macaddress_${interface}
-	$host-domain = $::domain
+	$host_mac = $::macaddress_interface
+	$host_domain = $::domain
 	
-	file { "/etc/sysconfig/network-scripts/ifcfg-${interface}":
-		content =>  template( "hp_network_rpm/ifcfg-${interface}.erb" ),
+	file { "/etc/sysconfig/network-scripts/ifcfg-$interface":
+		content =>  template( "hp_network_rpm/ifcfg-$interface.erb" ),
 		  owner => 'root',
 		  group => 'root',
 		 notify => Service['network'],
