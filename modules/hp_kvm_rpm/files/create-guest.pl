fa382cee64356a4f01db20e9d10b3aa0740241c4
berrak <bkronmailbox-git@yahoo.se>
2014-11-02 12:51:17 +0100

    Correct file name

--- modules/hp_kvm_rpm/files/create-guest.pl
+++ modules/hp_kvm_rpm/files/create-guest.pl
@@ -25,7 +25,7 @@ my $new_ip = $ARGV[2];
 system("virt-clone -o tpl_$domain -n $domain --mac $new_mac -f $out_image_path");
 
 # 2. Update the domain configuration with ip address and mac address
-my $xmlfile = $domain . ".xml";
+my $xmlfile = "tpl_" . $domain . ".xml";
 my $xmlpathfile = "/etc/libvirt/qemu/" . $xmlfile ;
 
 my $twig = XML::Twig->new(
