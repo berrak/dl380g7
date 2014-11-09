46643d851ac3b0394f1b52abdfbc82f3d836c2b6
berrak <bkronmailbox-git@yahoo.se>
2014-11-09 21:01:49 +0100

    Test file existence

--- modules/hp_apache2/manifests/install.pp
+++ modules/hp_apache2/manifests/install.pp
@@ -9,7 +9,7 @@ class hp_apache2::install {
     exec { "mod_header_install" :
            path => '/bin:/sbin:/usr/bin:/usr/sbin',
         command => 'a2enmod mod_headers',
-         unless =>  '/etc/apache2/mods-enabled/headers.load',
+        creates => '/etc/apache2/mods-enabled/headers.load',
         require => Package ["apache2-mpm-prefork"],
     }
     
