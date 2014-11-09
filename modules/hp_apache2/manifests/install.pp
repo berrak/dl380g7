6f7a2cda49d3f52ba7cb542ecb8684abdcdbfaf8
berrak <bkronmailbox-git@yahoo.se>
2014-11-09 20:56:32 +0100

    Add mod_headers

--- modules/hp_apache2/manifests/install.pp
+++ modules/hp_apache2/manifests/install.pp
@@ -4,5 +4,13 @@
 class hp_apache2::install {
 
     package { "apache2-mpm-prefork" : ensure => installed }
-
+    
+    # Requires mod_headers to be enabled (see security)
+    exec { "mod_header_install" :
+           path => '/bin:/sbin:/usr/bin:/usr/sbin',
+        command => 'a2enmod mod_headers',
+         unless =>  '/etc/apache2/mods-enabled/headers.load',
+        require => Package ["apache2-mpm-prefork"],
+    }
+    
 }
