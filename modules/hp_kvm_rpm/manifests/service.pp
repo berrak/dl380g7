76bb64037c1e117c56bfb0fa3e82185a1b3f0653
berrak <bkronmailbox-git@yahoo.se>
2014-11-22 19:33:23 +0100

    Enable default network

--- modules/hp_kvm_rpm/manifests/service.pp
+++ modules/hp_kvm_rpm/manifests/service.pp
@@ -7,6 +7,13 @@ class hp_kvm_rpm::service {
 			ensure => running,
 			enable => true,
 		   require => Package["libvirt"],
+		    notify => Exec["Enable_default_network"],
 	}
+	
+	exec { "Enable_default_network" :
+		       path => '/root/bin:/bin:/sbin:/usr/bin:/usr/sbin',
+		    command => "virsh net-start default && virsh net-autostart default",
+		  subscribe => Service["libvirtd"],
+	}	
 	
 }
