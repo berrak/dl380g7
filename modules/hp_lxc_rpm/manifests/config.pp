24d1b4d44c0d5fd85017cf6cb5c7c8b79b07257c
berrak <bkronmailbox-git@yahoo.se>
2014-12-03 08:04:06 +0100

    Correct resource name

--- modules/hp_lxc_rpm/manifests/config.pp
+++ modules/hp_lxc_rpm/manifests/config.pp
@@ -5,7 +5,7 @@ class hp_lxc_rpm::config {
 
 	# default configuration for new container creation
 	file { '/etc/lxc/default.conf' :
-		source => "puppet:///modules/hp_kvm_rpm/default.conf",
+		source => "puppet:///modules/hp_lxc_rpm/default.conf",
 		 owner => 'root',
 		 group => 'root',
 	}
