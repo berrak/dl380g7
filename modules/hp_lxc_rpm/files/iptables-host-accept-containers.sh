018a7382984ef3317536a78b4dc7fed7e29f5c7e
berrak <bkronmailbox-git@yahoo.se>
2014-12-04 21:01:22 +0100

    Add all containers DNAT

--- modules/hp_lxc_rpm/files/iptables-host-accept-containers.sh
+++ modules/hp_lxc_rpm/files/iptables-host-accept-containers.sh
@@ -9,7 +9,7 @@
 ###############################################################
 # ACCEPT RULES BEGINS HERE
 ###############################################################
-# puppet server listen port 8140 from container agents
+# puppet server listen port 8140 from all container agents
 iptables -L -v --line-numbers | grep dpt:8140
 if [ "$?" != "0" ] ; then
     iptables -I INPUT 1 -i virbr0 -p tcp -m tcp --dport 8140 -m state --state NEW -j ACCEPT
@@ -18,4 +18,29 @@ fi
 iptables -L -v --line-numbers | grep dpt:http
 if [ "$?" != "0" ] ; then
     iptables -I FORWARD 1 -i eth0 -o virbr0 -p tcp -m tcp --dport 80 -m state --state NEW -j ACCEPT
+fi
+# pre-route traffic to pseudo-eth0 (192.168.0.40) to correct container eth0 interface
+iptables -t nat -L -v --line-numbers | grep to:192.168.122.40
+if [ "$?" != "0" ] ; then
+    iptables -t nat -A PREROUTING -d 192.168.0.40 -j DNAT --to-destination 192.168.122.40
+fi
+# pre-route traffic to pseudo-eth0 (192.168.0.41) to correct container eth0 interface
+iptables -t nat -L -v --line-numbers | grep to:192.168.122.41
+if [ "$?" != "0" ] ; then
+    iptables -t nat -A PREROUTING -d 192.168.0.41 -j DNAT --to-destination 192.168.122.41
+fi
+# pre-route traffic to pseudo-eth0 (192.168.0.42) to correct container eth0 interface
+iptables -t nat -L -v --line-numbers | grep to:192.168.122.42
+if [ "$?" != "0" ] ; then
+    iptables -t nat -A PREROUTING -d 192.168.0.42 -j DNAT --to-destination 192.168.122.42
+fi
+# pre-route traffic to pseudo-eth0 (192.168.0.43) to correct container eth0 interface
+iptables -t nat -L -v --line-numbers | grep to:192.168.122.43
+if [ "$?" != "0" ] ; then
+    iptables -t nat -A PREROUTING -d 192.168.0.43 -j DNAT --to-destination 192.168.122.43
+fi
+# pre-route traffic to pseudo-eth0 (192.168.0.44) to correct container eth0 interface
+iptables -t nat -L -v --line-numbers | grep to:192.168.122.44
+if [ "$?" != "0" ] ; then
+    iptables -t nat -A PREROUTING -d 192.168.0.44 -j DNAT --to-destination 192.168.122.44
 fi
