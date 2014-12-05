5fa4bf5547e4d1673be0cec095ed0f5adb82530d
berrak <bkronmailbox-git@yahoo.se>
2014-12-05 10:32:12 +0100

    Add iptables rule at host for ssh

--- modules/hp_lxc_rpm/files/iptables-host-accept-containers.sh
+++ modules/hp_lxc_rpm/files/iptables-host-accept-containers.sh
@@ -7,18 +7,13 @@
 #
 #
 ###############################################################
-# ACCEPT RULES BEGINS HERE
+# ACCEPT RULES BEGINS HERE (ADDITIONAL IPTABLES RULES)
 ###############################################################
-# puppet server listen port 8140 from all container agents
-iptables -L -v --line-numbers | grep dpt:8140
-if [ "$?" != "0" ] ; then
-    iptables -I INPUT 1 -i virbr0 -p tcp -m tcp --dport 8140 -m state --state NEW -j ACCEPT
-fi
-# accept incoming public http/DNAT traffic to containers
-iptables -L -v --line-numbers | grep dpt:http
-if [ "$?" != "0" ] ; then
-    iptables -I FORWARD 1 -i eth0 -o virbr0 -p tcp -m tcp --dport 80 -m state --state NEW -j ACCEPT
-fi
+#
+## ============================================================
+##   PRE/POST ROUTING NAT
+## ============================================================
+#
 # pre-route traffic to pseudo-eth0 (192.168.0.40) to correct container eth0 interface
 iptables -t nat -L -v --line-numbers | grep to:192.168.122.40
 if [ "$?" != "0" ] ; then
@@ -44,3 +39,30 @@ iptables -t nat -L -v --line-numbers | grep to:192.168.122.44
 if [ "$?" != "0" ] ; then
     iptables -t nat -A PREROUTING -d 192.168.0.44 -j DNAT --to-destination 192.168.122.44
 fi
+
+## ============================================================
+##   LXC TRAFFIC (CONTAINER OUTBOUND TO HOST)
+## ============================================================
+
+# puppet server listen port 8140 from all container agents
+iptables -L -v --line-numbers | grep dpt:8140
+if [ "$?" != "0" ] ; then
+    iptables -I INPUT 1 -i virbr0 -p tcp -m tcp --dport 8140 -m state --state NEW -j ACCEPT
+fi
+
+## ============================================================
+##  DANGER *** INBOUND TRAFFIC (TO CONTAINERS) *** DANGER
+## ============================================================
+
+# accept incoming public ssh/DNAT traffic to containers
+iptables -L -v --line-numbers | grep dpt:ssh
+if [ "$?" != "0" ] ; then
+    iptables -I FORWARD 1 -o virbr0 -p tcp -m tcp --dport 22 -m state --state NEW -j ACCEPT
+fi
+
+# accept incoming public http/DNAT traffic to containers
+iptables -L -v --line-numbers | grep dpt:http
+if [ "$?" != "0" ] ; then
+    iptables -I FORWARD 1 -o virbr0 -p tcp -m tcp --dport 80 -m state --state NEW -j ACCEPT
+fi
+
