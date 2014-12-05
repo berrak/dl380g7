196d43917c98ca42e2a3793522cc44f623db06fa
berrak <bkronmailbox-git@yahoo.se>
2014-12-05 15:22:04 +0100

    Add domain specific noindex test page to httpd

--- modules/hp_apache2_rpm/manifests/config.pp
+++ modules/hp_apache2_rpm/manifests/config.pp
@@ -17,4 +17,13 @@ class hp_apache2_rpm::config {
         require => Package["httpd"],
     }
     
+    # customize the initial error-no-index file
+    file { "/container/$myhostname/rootfs/var/www/error/noindex.html" :
+        content =>  template( "hp_apache2_rpm/$myhostname.noindex.html.erb" ),    
+          owner => 'root',
+          group => 'root',
+           mode => '0644',
+        require => Package["httpd"],
+    } 
+    
 }
