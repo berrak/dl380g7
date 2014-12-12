abceac502840f7b8e76cfb443e49d375baad5212
berrak <bkronmailbox-git@yahoo.se>
2014-12-12 16:44:53 +0100

    Add some useful aliases to root

--- modules/hp_kvm_deb/manifests/config.pp
+++ modules/hp_kvm_deb/manifests/config.pp
@@ -10,28 +10,12 @@ class hp_kvm_deb::config {
 		group => 'root',
 	}	
 	
-	# create a subdirectory for vm images
+	# create a subdirectory for vm images (which already is LVM)
 	file { "/data/vm-images":
 		 ensure => "directory",
 		 owner => 'root',
 		 group => 'root',
 		require => File['/data'],
-	}
-	
-	# create the pool for storage of our vm images
-	exec { 'create_pool_vm_images' :
-		   path => "/bin:/sbin:/usr/bin:/usr/sbin",
-		command => "virsh pool-create-as vm-images dir --target /data/vm-images",
-		 unless => "virsh pool-list | grep vm-images",
-		require => File['/data/vm-images'],
-	}
-	
-	# auto-start pool at boot
-	exec { 'auto_start_pool_vm_images' :
-		   path => "/bin:/sbin:/usr/bin:/usr/sbin",
-		command => "virsh pool-autostart vm-images",
-		 unless => "virsh pool-list | grep vm-images | grep yes",
-		require => File['/data/vm-images'],
 	}
 	
     # Disable 'default' network
