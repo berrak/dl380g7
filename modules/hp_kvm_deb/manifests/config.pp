3abede51272ab20de80b166cb28ea4a57f4a659f
berrak <bkronmailbox-git@yahoo.se>
2014-12-12 15:23:49 +0100

    Create a storage pool for vm images

--- modules/hp_kvm_deb/manifests/config.pp
+++ modules/hp_kvm_deb/manifests/config.pp
@@ -4,13 +4,28 @@
 class hp_kvm_deb::config {
 
 
-
-
-
-
-
-
-
+	# set up storage pool
+	file { '/data':
+		owner => 'root',
+		group => 'root',
+	}	
+	
+	# create a subdirectory for vm images
+	file { "/data/vm-images":
+		 ensure => "directory",
+		 owner => 'root',
+		 group => 'root',
+		require => File['/data'],
+	}
+	
+	# create the pool for storage of our vm images
+	exec { 'create_pool_vm_images' :
+		   path => "/bin:/sbin:/usr/bin:/usr/sbin",
+		command => "virsh pool-create-as vm-images dir --target /data/vm-images",
+		 unless => "virsh pool-list | grep vm-images",
+		require => File['/data/vm-images'],
+	}
+	
 
     # Disable 'default' network
 	# run this only if 'default' is already started  
