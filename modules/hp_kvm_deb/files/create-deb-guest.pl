cdee06a34dea13771977d2f4eab6747d95418a9d
berrak <bkronmailbox-git@yahoo.se>
2014-12-13 14:58:42 +0100

    Type

--- modules/hp_kvm_deb/files/create-deb-guest.pl
+++ modules/hp_kvm_deb/files/create-deb-guest.pl
@@ -19,13 +19,13 @@ use Log::Log4perl;
 # initilize logger
 my $log_conf = q(
     log4perl.rootLogger              = DEBUG, LOG1
-    log4perl.appender.LOG1           = Log::Log4Perl::Appender::File
+    log4perl.appender.LOG1           = Log::Log4perl::Appender::File
     log4perl.appender.LOG1.filename  = /var/log/perl.log
     log4perl.appender.LOG1.mode      = append
-    log4perl.appender.LOG1.layout    = Log::Log4Perl::Layout::PatternLayout
+    log4perl.appender.LOG1.layout    = Log::Log4perl::Layout::PatternLayout
     log4perl.appender.LOG1.layout.ConversionPattern = %d %p %m %n   
 );
-Log::Log4Perl::init(\$log_conf);
+Log::Log4perl::init(\$log_conf);
 
 my $logger = Log::Log4perl->get_logger();
 
