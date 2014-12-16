75e73cb2998dc6db6ed3ad800b3287aa0f568abb
berrak <bkronmailbox-git@yahoo.se>
2014-12-16 12:34:32 +0100

    Correct test for recognized operatingsystem

--- modules/hp_dnsmasq/manifests/install.pp
+++ modules/hp_dnsmasq/manifests/install.pp
@@ -8,18 +8,10 @@ class hp_dnsmasq::install {
         allow_virtual => true,
     }
 
-    # ensure service is enabled at boot
-    if $::lsbdistid == 'OracleServer'  {
-        
-        exec { 'Enable_OL6_dnsmasq_at_boot':
-		command => '/sbin/chkconfig dnsmasq on',
-		 path   => '/usr/bin:/usr/sbin:/bin:/sbin',
-		require => Package['dnsmasq'],
-         onlyif => "chkconfig | grep 'dnsmasq ' | grep 3:off",
-        }
-          
-    } else {
-    	fail("FAIL: Unknown $::lsbdistid distribution. Aborting...")
+    # ensure recognized opearting system
+    if ( $::operatingsystem != in ['OracleLinux', 'Debian' ] ) {
+
+    	fail("FAIL: Unknown $::operatingsystem distribution. Aborting...")
     }
 
 }
