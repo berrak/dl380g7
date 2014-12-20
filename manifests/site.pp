bdd06e2c833bf77eb4a40d8c9e36eac774b4c13f
berrak <bkronmailbox-git@yahoo.se>
2014-12-20 23:01:26 +0100

    Add iptables to host

--- manifests/site.pp
+++ manifests/site.pp
@@ -756,6 +756,17 @@ node 'deborg.home.tld' {
 
     ## SECURITY
 
+	# Security (iptables + fail2ban)
+	# fail2ban ssh is enabled. disabled apache, modsec, postfix actions
+	# latter parameters needs both apache and mod-security installed
+    class { hp_iptables_fail2ban::config :
+		 puppetserver_hostname => 'hp',
+		   fail2ban_trusted_ip => '192.168.0.0/24  81.237.0.0/16',
+		       fail2ban_apache => 'true',
+		       fail2ban_modsec => 'true',
+			  fail2ban_postfix => 'false',
+	}
+
     # Automatic security upgrades with cron script
 	include hp_auto_upgrade
 	
