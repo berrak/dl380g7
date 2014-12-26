039e8a914b55789f6ce3528b5b05945d42441abc
berrak <bkronmailbox-git@yahoo.se>
2014-12-26 22:57:33 +0100

    add spice protocol server for remote install

--- modules/hp_kvm_deb/manifests/install.pp
+++ modules/hp_kvm_deb/manifests/install.pp
@@ -4,7 +4,7 @@
 class hp_kvm_deb::install {
 
     package { ["qemu-kvm", "libvirt-bin", "virtinst", "libguestfs-tools",
-	           "liblog-log4perl-perl", "libxml-twig-perl" ] :
+	           "liblog-log4perl-perl", "libxml-twig-perl", "libspice-server1" ] :
                ensure => present,
         allow_virtual => true,
     }
