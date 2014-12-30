fae14e89df71314c1c9f6c4adc58b67456e226ef
berrak <bkronmailbox-git@yahoo.se>
2014-12-30 09:13:29 +0100

    Correct local host name

--- manifests/site.pp
+++ manifests/site.pp
@@ -562,7 +562,7 @@ node 'hp.home.tld' {
 	hp_kvm_deb::add_guest { 'debse' :
           local_guest_gw => '192.168.0.1', local_guest_ip  => '192.168.0.45', local_mac_address => '52:54:00:00:00:45',
 		local_guest_bcst => '192.168.0.255', local_guest_netw => '192.168.0.0',
-		 local_hostname  => 'ora', bridge_name => 'kvmbr0', auto_start => 'true',
+		 local_hostname  => 'debse', bridge_name => 'kvmbr0', auto_start => 'true',
 		 os_name => 'oracle6', local_guest_netmask => '255.255.255.0', local_guest_domain => 'home.tld',
     }
 	
