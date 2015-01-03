ffc11971b96021388c08bcd7369d6b909037513a
berrak <bkronmailbox-git@yahoo.se>
2015-01-03 20:44:10 +0100

    Use puppet user type only

--- modules/hp_add_git_project/manifests/config.pp
+++ modules/hp_add_git_project/manifests/config.pp
@@ -10,20 +10,12 @@ define hp_add_git_project::config {
 		fail("FAIL: This module is only for Debian, not for $::operatingsystem. Aborting...")
 	}
 
-    # create user/group with name of project
-	exec { "create_project_${name}" :
-		   path => '/bin:/sbin:/usr/bin:/usr/sbin',
-		command => "useradd --shell /usr/bin/git-shell $name", 
-		 unless => "cat /etc/passwd | grep $name",
-		 notify => User["$name"],
-	}
-	
-	# add password to this 'user'
+	# add user and password to this 'user'
 	$password = $name
 	user { $name :
 		   ensure => present,
+		    shell => '/usr/bin/git-shell',
 		 password => generate('/bin/sh', '-c', "mkpasswd -m sha-512 ${password} | tr -d '\n'"),
-		subscribe => Exec["create_project_${name}"],
 	}
 	
 }
